8152f8cfafd79eb4d0ce46be283edb49afa33c36
Volan Nnanpalle <vkn19614@huskies.bloomu.edu>
2017-06-16 19:33:49 +0000

    complete website change

---
+++ js/mail.js
@@ -0,0 +1,29 @@
+$(function($){
+
+	$("form").submit(function(event) {
+
+		event.preventDefault();
+
+		$.ajax({
+	    url: "https://formspree.io/nnanpalle@gmail.com", 
+	    method: "POST",
+	    data: {
+	    	name: $("#name").val(),
+	    	email: $("#email").val(),
+	    	phone: $("#phone").val(),
+	    	message: $("#message").val()
+	    },
+	    dataType: "json"
+		}).done(function(){
+			$("#name").val("");
+			$("#email").val("");
+			$("#phone").val("");
+			$("#message").val("");
+			document.getElementById("success").innerHTML="Message Sent"
+			// alert("Message Sent!");
+		}).fail(function(){
+			alert("ERROR WHILE SENDING MESSAGE!");
+		});
+	});
+
+}) ;
