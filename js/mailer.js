5169d9327b86bbfecd04639660ffe1d64ed5801a
Volan Nnanpalle <vkn19614@huskies.bloomu.edu>
2017-06-19 04:42:59 +0000

    changed mailer

--- js/mailer.js
+++ js/mailer.js
@@ -5,7 +5,7 @@ $(function($){
 		event.preventDefault();
 
 		$.ajax({
-	    url: "https://formspree.io/volankang@hotmail.com", 
+	    url: "https://formspree.io/vnnanpalle@hotmail.com", 
 	    method: "POST",
 	    data: {
 	    	name: $("#name").val(),
